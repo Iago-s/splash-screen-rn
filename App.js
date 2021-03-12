902b2d02fbb19fb821ec4ebabba63f7ab3cd0a63
Iago <iagooliveira3845@hotmail.com>
2021-03-12 12:26:26 -0300

    Splash screen no android

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Splash screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000',
+  }
+});
+
+export default App;
